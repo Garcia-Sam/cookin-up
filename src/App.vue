7e930285c8d8187a0bf716e3d3639f55253db2fc
Samuel Gracia <macedosam55@gmail.com>
2024-05-15 12:40:12 -0300

    Upload All

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: { Banner } // quando o nome da propriedade é igual ao seu valor, podemos deixar só um { Banner: Banner } 
+}
+</script>
+
+<template>
+  <Banner />
+</template>
+
+<style scoped>
+
+</style>
+
