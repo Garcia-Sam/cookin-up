228ea3c12c365697cb6fca0cc5b7ed5568b90fb1
Samuel Gracia <macedosam55@gmail.com>
2024-05-16 11:44:14 -0300

    Create ConteudoPrincipal.vue and update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
 
 export default {
-  components: { Banner } // quando o nome da propriedade é igual ao seu valor, podemos deixar só um { Banner: Banner } 
+  components: { Banner, ConteudoPrincipal } // quando o nome da propriedade é igual ao seu valor, podemos deixar só um { Banner: Banner } 
 }
 </script>
 
 <template>
   <Banner />
+  <ConteudoPrincipal />
 </template>
 
 <style scoped>
