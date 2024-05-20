575d8ce7deb4d3d41ed501da7d42783f895fc8c3
Samuel Gracia <macedosam55@gmail.com>
2024-05-20 11:52:08 -0300

    Upload new images to public folder

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -10,6 +10,9 @@ export default {
 </script>
 
 <template>
-    {{ categoria.nome }}
-    {{ categoria.imagem }}
+    <article class="categoria">
+        <header class="categoria__cabecalho">
+            <img src="" alt="" class="categoria__imagem">
+        </header>
+    </article>
 </template>
