39cee66daa6c1d13e1fe7ea2d8ba852c1412ac90
Samuel Gracia <macedosam55@gmail.com>
2024-05-22 11:29:19 -0300

    Create Rodape.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
 import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+import Rodape from './components/Rodape.vue';
 
 export default {
-  components: { Banner, ConteudoPrincipal } // quando o nome da propriedade é igual ao seu valor, podemos deixar só um { Banner: Banner } 
+  components: { Banner, ConteudoPrincipal, Rodape } // quando o nome da propriedade é igual ao seu valor, podemos deixar só um { Banner: Banner } 
 }
 </script>
 
 <template>
   <Banner />
   <ConteudoPrincipal />
+  <Rodape />
 </template>
 
 <style scoped>
